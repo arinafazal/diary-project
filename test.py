7334bffea018154a261de16e5a1ca6bdf8d8a7b5
unknown <arinafazal95@gmail.com>
2023-02-24 12:45:12 +0700

    freeze dotenv and other requirements

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-now = datetime.now()
-print(now)
-
-date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
-print(date_time)
--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
-import certifi
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
 
 connection_string = 'mongodb+srv://admin:arina@cluster0.yqoaapn.mongodb.net/?retryWrites=true&w=majority'
 client = MongoClient(connection_string)
